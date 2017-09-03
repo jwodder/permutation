8971ec25cf726e2cd5d6ffffcc430167b49bfc40
John T. Wodder II <git@varonathe.org>
2017-09-03 19:09:55 +0000

    Test `next_permutation` and `prev_permutation`

--- test/test_s_n.py
+++ test/test_s_n.py
@@ -1,5 +1,6 @@
-from six.moves   import zip_longest
-from permutation import Permutation
+import pytest
+from   six.moves   import zip_longest
+from   permutation import Permutation
 
 S4 = [
     Permutation.identity(),
@@ -32,3 +33,17 @@ def test_s4():
     for i,(p,q) in enumerate(zip_longest(Permutation.s_n(4), S4)):
         assert p == q
         assert p.modified_lehmer() == i
+
+def test_next_permutation():
+    for i in range(len(S4)-1):
+        assert S4[i].next_permutation() == S4[i+1]
+        assert S4[i].modified_lehmer()+1 == S4[i+1].modified_lehmer()
+
+def test_prev_permutation():
+    for i in range(len(S4)-1):
+        assert S4[i+1].prev_permutation() == S4[i]
+        assert S4[i+1].modified_lehmer()-1 == S4[i].modified_lehmer()
+
+def test_prev_permutation_identity():
+    with pytest.raises(ValueError):
+        Permutation.identity().prev_permutation()
