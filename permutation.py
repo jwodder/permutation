e58d22dd7efa37618721b76bc0c34bbbc147ec6b
John T. Wodder II <git@varonathe.org>
2020-03-13 21:00:25 +0000

    Switch from pytest-flakes to flake8

--- permutation.py
+++ permutation.py
@@ -457,7 +457,7 @@ class Permutation(object):
                 map2[i], map2[j] = map2[j], map2[i]
                 map2[:i] = reversed(map2[:i])
                 return type(self)(*map2)
-        assert False  # pragma: no cover
+        raise AssertionError('Unreachable state reached')  # pragma: no cover
 
     @classmethod
     def group(cls, n):
